eca898292b20b2c9aee95c43ce696f0a04df128c
Yuriy Bakhtin <yurybakh@gmail.com>
2026-01-23 13:51:37 +0300

    Increase module version on update min HumHub version by rector

--- src/HumHubUtils/UpdateHumHubMinVersion.php
+++ src/HumHubUtils/UpdateHumHubMinVersion.php
@@ -18,6 +18,10 @@ class UpdateHumHubMinVersion
                     $moduleJson['humhub']['maxVersion'] = $minHumHubVersion;
                 }
 
+                if (isset($moduleJson['version'])) {
+                    $moduleJson['version'] = self::increaseVersionLevel($moduleJson['version']);
+                }
+
                 file_put_contents(
                     $moduleJsonPath,
                     json_encode($moduleJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES),
@@ -42,5 +46,32 @@ class UpdateHumHubMinVersion
         // Normalize a beta version e.g. from 1.18-beta.6 to 1.18.0.6,
         // because version_compare() decides version 1.18 as stable and higher than beta.
         return preg_replace('/[^0-9.]+/', '.0', $version);
+    }
+
+    /**
+     * Increase a version level
+     *
+     * Examples:
+     *  - 1.8.4 => 2.0.0 (when $level = 1)
+     *  - 1.8.4 => 1.9.0 (when $level = 2)
+     *  - 1.8.4 => 1.8.5 (when $level = 3)
+     *
+     * @param string $version
+     * @param int $level
+     * @return string
+     */
+    private static function increaseVersionLevel(string $version, int $level = 2): string
+    {
+        $version = explode('.', $version);
+
+        for ($l = 0; $l < count($version); $l++) {
+            if ($l === $level - 1) {
+                $version[$l] = (int) $version[$l] + 1;
+            } elseif ($l >= $level) {
+                $version[$l] = 0;
+            }
+        }
+
+        return implode('.', $version);
     }
 }
